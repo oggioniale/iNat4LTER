3126c7c33ee0b6afec8589bac497105199457dfc
Alessandro Oggioni <oggioniale@gmail.com>
2024-04-22 11:52:11 +0200

    script for test

---
+++ test.R
@@ -0,0 +1,3 @@
+# iNat script
+# for download observations from iNat projects
+gransasso <- rinat::get_inat_obs_project("lter-appennino-centrale-gran-sasso-d-italia", type = "observations", raw = TRUE)
